2c311bd6a0682813a00bc048a543f45b2dbd621d
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-02 21:34:49 +0800

    Inline Styling for React Elements

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "blue",
+  border: "1px solid black",
+  fontSize: "20px"
+};
+
+customStyle.color = "red";
+
 ReactDOM.render(
-  <div>
-    <h1 className="heading">My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <img
-        className="food-img"
-        alt="bacon"
-        src="https://storage.googleapis.com/gen-atmedia/3/2010/02/55cc6ae010585eb7933c5f96dea94f637330cff4.jpeg"
-      />
-      <li>Jamon</li>
-      <img
-        className="food-img"
-        alt="jamon"
-        src="https://www.otiendas.com/wp-content/uploads/2017/10/Jamon-serrano.jpg"
-      />
-      <li>Noodles</li>
-      <img
-        className="food-img"
-        alt="noodles"
-        src="https://www.seriouseats.com/recipes/images/2015/05/20150424-singapore-noodles-shao-zhong-20-1500x1125.jpg"
-      />
-    </ul>
-  </div>,
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
