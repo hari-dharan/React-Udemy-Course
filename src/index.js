56d328d0fe61aaf3fd2c777b5863562cf48aa3c8
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-03 14:59:46 +0800

    JS ES6 - Import, Export and Modules
    Using the * keyword to export everything from external file. However, it is not encouraged.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi } from "./math";
+import * as pi from "./math";
+
+console.log(pi);
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
