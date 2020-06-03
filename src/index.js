df58ece654911c24955d61199da2d02d5c85a1eb
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-03 14:58:05 +0800

    JS ES6 - Import, Export and Modules
    Understanding the export default.
    Exporting multiple objects.
    Importing the default.
    Importing multiple objects.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App";
+import pi, { doublePi, triplePi } from "./math";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
