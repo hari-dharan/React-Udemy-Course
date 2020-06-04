ff9ebbe87f2d0bdb1933a8ec91f473599a22f6d3
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-04 21:24:12 +0800

    The Map Function
    Replaced <Card {...contacts[0]} /> etc to a Map function which serves as a for loop instead of repetitive code.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import contacts from "../contacts";
 import Card from "./Card";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>My Contacts</h1>
-      <Card {...contacts[0]} />
-      <Card {...contacts[1]} />
-      <Card {...contacts[2]} />
+      {contacts.map(createCard)}
     </div>
   );
 }
