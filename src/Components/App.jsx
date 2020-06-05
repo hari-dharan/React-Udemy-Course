17aa9d2fa4b315dfa847eddad5f5102bcdc7c422
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-05 19:40:05 +0800

    React Hooks - useState

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,47 @@
 // {}
-import React from "react";
-import Login from "./Login";
-import Register from "./Register";
-
-var userIsRegistered = false;
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="container">
-      {userIsRegistered ? <Login /> : <Register />}
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
 
 export default App;
 
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
+/* 
+
+Notes on Basic Hooks from Docs
+
+// const [state, setState] = useState(initial State) 
+
+Returns a stateful value, and a function to update it.
+During the initial render, the returned state (state) is 
+the same as the value passed as the first argument
+(initialState).
+
+The setState function is used to update the state. 
+It accepts a new state value and enqueues a re-render 
+of the component.
+
+// setState(newState)
+
+During subsequent re-renders, the first value 
+returned by useState will always be the most 
+recent state after applying updates.
+
+*/
