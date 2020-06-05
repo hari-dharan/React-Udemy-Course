e80ada3bf6fd9d80fd3b55270f1f97356164cd85
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-05 18:28:07 +0800

    Using the null value in a Ternary Operator

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,9 +4,10 @@ import Login from "./Login";
 
 var isLoggedIn = false;
 
+const currentTime = new Date().getHours();
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">{currentTime > 12 ? <h1>Hello</h1> : null}</div>
   );
 }
 
