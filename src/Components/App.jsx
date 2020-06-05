b6818048da6d716beb3e82de0e612dc82abb8df2
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-05 18:42:06 +0800

    Conditional Rendering Practice

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,22 @@
 // {}
 import React from "react";
 import Login from "./Login";
+import Register from "./Register";
 
-var isLoggedIn = false;
+var userIsRegistered = false;
 
-const currentTime = new Date().getHours();
 function App() {
-  return <div className="container">{currentTime > 12 && <h1>Hello</h1>}</div>;
+  return (
+    <div className="container">
+      {userIsRegistered ? <Login /> : <Register />}
+    </div>
+  );
 }
 
 export default App;
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
