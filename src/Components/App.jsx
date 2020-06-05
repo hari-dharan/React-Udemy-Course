0ebbfac6029276b6090b7e5344b8235461badb8e
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-05 20:07:53 +0800

    useState Hook Practice

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,46 +2,20 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  setInterval(getTime, 1000);
+  const today = new Date().toLocaleTimeString();
+  const [state, setState] = useState(today);
 
-  function increase() {
-    setCount(count + 1);
+  function getTime() {
+    const newToday = new Date().toLocaleTimeString();
+    setState(newToday);
   }
-
-  function decrease() {
-    setCount(count - 1);
-  }
-
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <h1>{state}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
 
 export default App;
-
-/* 
-
-Notes on Basic Hooks from Docs
-
-// const [state, setState] = useState(initial State) 
-
-Returns a stateful value, and a function to update it.
-During the initial render, the returned state (state) is 
-the same as the value passed as the first argument
-(initialState).
-
-The setState function is used to update the state. 
-It accepts a new state value and enqueues a re-render 
-of the component.
-
-// setState(newState)
-
-During subsequent re-renders, the first value 
-returned by useState will always be the most 
-recent state after applying updates.
-
-*/
