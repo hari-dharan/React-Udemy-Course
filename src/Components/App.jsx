87ea3a4b481b108ced949149bddcc7df217e1273
hari-dharan <65288821+hari-dharan@users.noreply.github.com>
2020-06-06 21:21:31 +0800

    Changing Complex State Practice (Improved)
    Change if else if statements to ternary operators.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -12,13 +12,13 @@ function App() {
     const { value, name } = event.target;
 
     setContact(lastValue => {
-      if (name === "fName") {
-        return { fName: value, lName: lastValue.lName, email: lastValue.email };
-      } else if (name === "lName") {
-        return { fName: lastValue.fName, lName: value, email: lastValue.email };
-      } else if (name === "email") {
-        return { fName: lastValue.fName, lName: lastValue.lName, email: value };
-      }
+      return name === "fName"
+        ? { fName: value, lName: lastValue.lName, email: lastValue.email }
+        : name === "lName"
+        ? { fName: lastValue.fName, lName: value, email: lastValue.email }
+        : name === "email"
+        ? { fName: lastValue.fName, lName: lastValue.lName, email: value }
+        : null;
     });
   }
 
